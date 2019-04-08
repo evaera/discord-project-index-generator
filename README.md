48ceaab4c3de8a985a185e867a76be38b0241a72
Eryn Lynn <eryn@eryn.io>
2019-04-08 01:41:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Discord Project Index Generator
+
+Generates a Discord web hook embed of your projects in a server!
+
+Clone & change data folder and rename .env.example to .env and set the values
---
+++ src/index.ts
@@ -0,0 +1,107 @@
+import fs from 'fs'
+import path from 'path'
+import yaml from 'js-yaml'
+import fetch from 'node-fetch'
+
+require('dotenv').config()
+
+const channels = require('../data/channels.json')
+
+interface Project {
+  name?: string
+  channel?: string
+  repo?: string
+  link?: string
+  desc?: string
+}
+
+async function getColors () {
+  return fetch('https://raw.githubusercontent.com/ozh/github-colors/master/colors.json').then(res => res.json())
+}
+
+async function main () {
+  const projects = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '../data/projects.yml'), 'utf8')) as Project[]
+
+  // Alphabetize
+  projects.sort((a, b) => {
+    const first = a.name || a.repo || ''
+    const second = b.name || b.repo || ''
+
+    return first > second ? 1 : -1
+  })
+
+  const colors = await getColors()
+
+  const embeds = await Promise.all(projects.map(async project => {
+    let { name, channel, repo, link, desc } = project
+
+    if (!name && !repo) {
+      throw new Error('Must specify one of: name, repo')
+    }
+
+    name = name || repo
+
+    if (channel) {
+      if (!channels[channel]) {
+        throw new Error(`Unknown channel ${channel}`)
+      }
+
+      channel = `<#${channels[channel]}>`
+    }
+
+    if (repo && !repo.includes('/')) {
+      repo = `${process.env.GITHUB_USERNAME!}/${repo}`
+    }
+
+    let language: string | undefined
+    let repoUrl: string | undefined
+    let color = 0x95a5a6
+
+    if (repo) {
+      const doc = await fetch(`https://api.github.com/repos/${repo}`).then(res => res.json())
+
+      language = doc.language
+      link = link || doc.homepage
+      desc = desc || doc.description
+      repoUrl = `https://github.com/${repo}`
+    }
+
+    if (language && colors[language]) {
+      color = Number(colors[language].color.replace('#', '0x'))
+    }
+
+    return {
+      title: name,
+      description: [
+        channel || '',
+        desc,
+        [
+          link ? `[Homepage](${link})` : '',
+          link && repo ? ' | ' : '',
+          repo ? `[GitHub](${repoUrl})` : ''
+        ].join('')
+      ].join('\n\n'),
+      color,
+      url: link || repoUrl,
+      fields: [
+
+      ]
+    }
+  }))
+
+  await fetch(process.env.WEBHOOK_URL!, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      avatar_url: process.env.WEBHOOK_AVATAR!,
+      username: 'Projects Index',
+      embeds
+    })
+  })
+}
+
+main().catch(e => {
+  throw e
+})
